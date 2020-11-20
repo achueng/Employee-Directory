f89538d870ced0ece58c2b492f6b72faca5837bb
Agnes Chueng <agneschueng@gmail.com>
2020-11-19 22:49:37 -0600

    add layout of components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Search from "./components/Search";
+import Table from "./components/Table";
 
 function App() {
   return (
-    <Header/>
+    <React.Fragment>
+      <Header/>
+      <Container>
+        <Row>
+            <Search/>
+        </Row>
+        <Row>
+            <Table/>
+        </Row>
+      </Container>
+    </React.Fragment>
   );
 }
 
