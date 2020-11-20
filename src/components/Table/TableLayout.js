7b9e815054137059854d522aa20bc39724e4122a
Agnes Chueng <agneschueng@gmail.com>
2020-11-19 23:21:26 -0600

    format table with correct labels for data

--- src/components/Table/TableLayout.js
+++ src/components/Table/TableLayout.js
@@ -1,5 +1,28 @@
-function TableLayout() {
-    return <h1>TABLE GOES HERE</h1>
+import Table from 'react-bootstrap/Table'
+
+function TableLayout(props) {
+    return (
+        <Table striped bordered responsive>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>City</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><img src=""/></td>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>City</td>
+                    <td>Age</td>
+                </tr>
+            </tbody>
+        </Table>
+    )
 }
 
 export default TableLayout;
