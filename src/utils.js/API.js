4c4822fbf83f21542b2769d95fd825c7e53c520a
Agnes Chueng <agneschueng@gmail.com>
2020-11-19 23:21:04 -0600

    reduce search results to 20

--- src/utils.js/API.js
+++ src/utils.js/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     employees: function() {
-        return axios.get("https://randomuser.me/api/?results=200");
+        return axios.get("https://randomuser.me/api/?results=20");
     }
 }
