a167a302160ac7d11a9bc22d8768ae4d286be38d
Agnes Chueng <agneschueng@gmail.com>
2020-11-19 23:45:55 -0600

    set up api call and get the select data

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,6 +1,31 @@
+import React, { useEffect, useState } from "react";
 import TableLayout from "./TableLayout";
+import API from "../../utils.js/API";
 
 function Table() {
+    const [employeesState, setEmployeesState] = useState([]);
+
+    useEffect(() => {
+        getEmployees();
+    }, []);
+
+    function getEmployees() {
+        API.employees()
+        .then(({data}) => {
+            const selectData = data.results.map(employee => {
+                return {
+                    image: employee.picture.thumbnail,
+                    name: `${employee.name.first} ${employee.name.last}`,
+                    email: employee.email,
+                    city: employee.location.city,
+                    age: employee.dob.age
+                }
+            })
+            setEmployeesState(selectData);
+        })
+        .catch(err => console.log(err))
+    }
+
     return <TableLayout/>
 }
 
