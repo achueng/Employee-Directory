fc48b3c394b2dfe796c4e2a9ad9f3a6483974722
Agnes Chueng <agneschueng@gmail.com>
2020-11-19 22:58:16 -0600

    modify webpage layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
 import Container from "react-bootstrap/Container";
-import Row from 'react-bootstrap/Row';
-import Search from "./components/Search";
 import Table from "./components/Table";
 
 function App() {
@@ -10,12 +8,7 @@ function App() {
     <React.Fragment>
       <Header/>
       <Container>
-        <Row>
-            <Search/>
-        </Row>
-        <Row>
-            <Table/>
-        </Row>
+          <Table/>
       </Container>
     </React.Fragment>
   );
--- src/components/Search/index.js
+++
@@ -1,5 +0,0 @@
-function Search() {
-    return <h3>SEARCH BAR GOES HERE</h3>
-}
-
-export default Search;
